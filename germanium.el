1dfc483c2e8655d0033eb19bbd533891fd277a56
matsuyoshi <sfbgwm30@gmail.com>
2021-08-21 14:30:20 +0900

    fix commentary

--- germanium.el
+++ germanium.el
@@ -23,9 +23,9 @@
 
 ;;; Commentary:
 
-;; Tool for generating image from source code using germanium.
-;; It requires you to have the `germanium' command-line tool
-;; installed on your path.
+;; Tool for generating image from source code using germanium
+;; <https://github.com/matsuyoshi30/germanium>.  It requires you
+;; to have the `germanium' command-line tool installed on your path.
 
 ;;; Code:
 
